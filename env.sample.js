bc18657bfdeab328ba529ffaca0d3505c865c481
Joe McIlhargey <jmcilhargey@gmail.com>
2016-12-15 21:43:45 -0800

    Create env.sample.js

---
+++ env.sample.js
@@ -0,0 +1,7 @@
+process.env["QUANDL_KEY"] = "";
+process.env["TWITTER_KEY"] = "";
+process.env["TWITTER_SECRET"] = "";
+process.env["TWITTER_SEARCH_TOKEN"] = "";
+process.env["TWITTER_OAUTH_TOKEN"] = "";
+process.env["TWITTER_OAUTH_SECRET"] = "";
+process.env["MLAB_URI"] = "";
